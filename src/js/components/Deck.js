10dd3952d764f52ff640d46efd5d83090e56688e
Jordan Neeb <js.neeb1780@gmail.com>
2017-08-18 08:22:45 -0700

    comments and keyGen

--- src/js/components/Deck.js
+++ src/js/components/Deck.js
@@ -1,45 +1,45 @@
 import React from "react";
 
 export default class Deck extends React.Component {
-	buildDeck() {
+	generateDeck() {
 
 		let gameDeck = [];
-		let numOfDecks = 1;
+		let numOfDecks = 2;		// TODO: set value to user input
 		let suits = ["Clubs", "Spades", "Hearts", "Diamonds"];
-		const faces = ["Jack", "Queen", "King", "Ace"]
+		const faces = ["Jack", "Queen", "King", "Ace"];
+		const pipValues = ["two", "three", "four", "five", "six", "seven", "eight", "nine", "ten"];
 
 		/* Create Pip Cards */
-		let pipValue = 1;
-		let index = 0;
+		let index = 0;		// faces && suits
+		let pipIndex = 0;
 		
 		for (var i = 0; i < (36 * numOfDecks); i++) {
-			if (i % 9 === 0) {
-				pipValue = 1;
-			}
-			if (i % 4 === 0) {
+			if (i % 4 === 0) {		// reset suits
 				index = 0;
 			}
-			pipValue++;
-			let key = Math.random();
-			let div = <div key={key} class={pipValue + suits[index]} data-points={pipValue} />
+			if (i % 9 === 0) {		// reset pipValues
+				pipIndex = 0;
+			}
+			let key = pipValues[pipIndex] + "_" + (i + 1);
+			let div = <div key={key} class={"card " + pipValues[pipIndex] + suits[index]} data-points={pipIndex + 2} />
 			gameDeck.push(div);
 			index++;
+			pipIndex++;
 		}
 
 		/* Create Face Cards */
-
 		for (var i = 0; i < ((faces.length * 4) * numOfDecks); i++) {
-			if (i % 4 === 0) {
+			if (i % 4 === 0) {		// reset suits && faces
 				index = 0;
-				suits.push(suits.shift());
+				suits.push(suits.shift());		// shuffle suits
 			}
-			let key = Math.random();
+			let key = faces[index] + "_" + (i + 1);
 			let div = "";
 			if (faces[index] !== "Ace") {
-				div = <div key={key} class={faces[index] + suits[index]} data-points={10} />
+				div = <div key={key} class={"card " + faces[index] + suits[index]} data-points={10} />
 			}
 			else {
-				div = <div key={key} class={faces[index] + suits[index]} data-points={11 || 1} />
+				div = <div key={key} class={"card " + faces[index] + suits[index]} data-points={11 || 1} /> // TODO: change "data-points" to function
 			}
 			gameDeck.push(div);
 			index++;
@@ -50,10 +50,10 @@ export default class Deck extends React.Component {
 	}
 
 	render() {
-		console.log(this.buildDeck());
+		console.log(this.generateDeck());
 		return (
-			<div id="deck">
-				{this.buildDeck()}
+			<div class="deck">
+				{this.generateDeck()}
 			</div>
 		);
 	}
