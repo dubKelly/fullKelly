3f22667e2107ff1ebd781a24e7425dfcd62a3319
Jordan Neeb <js.neeb1780@gmail.com>
2017-08-21 08:24:40 -0700

    added selected onClick

--- src/js/components/Setup/NumOfDecksInput.js
+++ src/js/components/Setup/NumOfDecksInput.js
@@ -2,20 +2,30 @@ import React from "react";
 
 export default class NumOfDecksInput extends React.Component {
 
-	selectDeckOption(e) {
-		const option = e.target;
-		this.props.setNumOfDecks(option.innerHTML);
-		option.classList.add("selected");
+	selectDeckOption = (e) => {
+		 
+		 const selected = e.target.innerHTML;
+		 this.props.setNumOfDecks(selected);
+
+		 const options = document.getElementsByClassName("deckOption");
+		 for (var i = options.length - 1; i >= 0; i--) {
+		 	if (options[i] !== e.target) {
+		 		options[i].classList.remove("selected");
+		 	}
+		 	else {
+		 		options[i].classList.add("selected");
+		 	}
+		 }
 	}
 
 	render() {
 		return (
 			<div>
-				<span className="deckOption" onClick={this.selectDeckOption.bind(this)}>1</span>
-				<span className="deckOption" onClick={this.selectDeckOption.bind(this)}>2</span>
-				<span className="deckOption" onClick={this.selectDeckOption.bind(this)}>4</span>
-				<span className="deckOption" onClick={this.selectDeckOption.bind(this)}>6</span>
-				<span className="deckOption" onClick={this.selectDeckOption.bind(this)}>8</span>
+				<span className="deckOption" onClick={this.selectDeckOption}>1</span>
+				<span className="deckOption" onClick={this.selectDeckOption}>2</span>
+				<span className="deckOption" onClick={this.selectDeckOption}>4</span>
+				<span className="deckOption" onClick={this.selectDeckOption}>6</span>
+				<span className="deckOption" onClick={this.selectDeckOption}>8</span>
 			</div>
 		);
 	}
